1107e327c057db2a5a9a797396d4b9c7b1a8d94b
Kirtkub <cleoyleo@proton.me>
2026-09-23 23:16:55 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const collectionAddress = "kQB2njpo7QGmTd0RmjeAwtRsKgjARp7XYeagI2MXfjVwdty9";
+export const collectionAddress = "kQAjFS5LpNarcJmrCRwlmH5SzR0GQ_elDoxmPspmVhKGTxDm";
 export const collectionMetadata = "https://astounding-flan-ffc457.netlify.app/collectionMetadata.json";
 export const nftsMetadataIndex = "https://astounding-flan-ffc457.netlify.app/metadataIndex.json";
 export const tonChain = "Testnet";
