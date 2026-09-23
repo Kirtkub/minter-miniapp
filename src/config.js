5b25864e214f4a54a1f09b7942d1328f34e20fdd
Kirtkub <cleoyleo@proton.me>
2026-09-23 20:34:50 +0200

    commit

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const collectionAddress = "kQDeZQGgQXsn6UK785TT9LVe2MTLZGHEx5jLqE8oOrKMEBHA";
-export const collectionMetadata = "https://astounding-flan-ffc457.netlify.app/collectionMetadata.json";
-export const nftsMetadataIndex = "https://astounding-flan-ffc457.netlify.app/metadataIndex.json";
-export const tonChain = "Testnet";
