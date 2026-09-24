7fbba34de65b963826ce41bb124fef8bffef42d3
Kirtkub <cleoyleo@proton.me>
2026-09-24 08:27:19 +0200

    new contract

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const collectionAddress = "kQAjFS5LpNarcJmrCRwlmH5SzR0GQ_elDoxmPspmVhKGTxDm";
+export const collectionAddress = "kQA3dRO7EJ6Efstyxr8l7Yo-JnnGdVh0ylDTZwprWA0iyZqW";
 export const collectionMetadata = "https://astounding-flan-ffc457.netlify.app/collectionMetadata.json";
 export const nftsMetadataIndex = "https://astounding-flan-ffc457.netlify.app/metadataIndex.json";
 export const tonChain = "Testnet";
