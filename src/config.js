acb010865a407142a13749d1990e86097f3d0bea
Kirtkub <cleoyleo@proton.me>
2026-09-24 12:48:46 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const collectionAddress = "kQA3dRO7EJ6Efstyxr8l7Yo-JnnGdVh0ylDTZwprWA0iyZqW";
+export const collectionAddress = "kQBUFysKjAy9FFKcp4Zw3BKVKovN3mwaoFdjYT92hFX50AsG";
 export const collectionMetadata = "https://astounding-flan-ffc457.netlify.app/collectionMetadata.json";
 export const nftsMetadataIndex = "https://astounding-flan-ffc457.netlify.app/metadataIndex.json";
 export const tonChain = "Testnet";
